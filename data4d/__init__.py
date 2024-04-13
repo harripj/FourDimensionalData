e77da55438173de76b82c4be1d7427f39f88da96
Paddy Harrison <pj.harrison31@gmail.com>
2024-04-13 11:37:03 -0300

    init tests

--- data4d/__init__.py
+++ data4d/__init__.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
 from .TVIPS import TVIPS
 from .base import FourDimensionalData
 from .blockfile import BLO
+
+__version__ = open(Path(__file__).parent.joinpath("VERSION")).read().strip()
+__all__ = ["FourDimensionalData", "BLO", "TVIPS"]
